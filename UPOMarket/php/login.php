fecd04d584ba7800a78d5580ce6a1fcca613a12d
Alvaro <alvaronavarromora@gmail.com>
2020-01-15 21:18:07 +0100

    Registro

--- UPOMarket/php/login.php
+++ UPOMarket/php/login.php
@@ -28,10 +28,6 @@
                                     <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
                                 </div>
                                 <br />
-                                <div class="custom-control custom-checkbox mb-3">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                    <label class="custom-control-label" for="customCheck1">Recordar contraseña</label>
-                                </div>
                                 <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Iniciar Sesión</button>
                             </form>
                         </div>
