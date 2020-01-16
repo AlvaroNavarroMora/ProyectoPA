68441c8350846b3abd82a851394c0e4aab5d6e91
Alvaro <alvaronavarromora@gmail.com>
2020-01-16 18:01:17 +0100

    Código de Registro y de Inicio de Sesión

--- UPOMarket/php/login.php
+++ UPOMarket/php/login.php
@@ -1,39 +1,58 @@
-<?php ?>
-<!DOCTYPE html>
+<?php
+include "./utils/sesionUtils.php";
+//Funciones
+function cargarLogin() {
+    ?>
+    <!DOCTYPE html>
 
-<html>
-    <head>
-        <title>Iniciar Sesión</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../frameworks/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link href="../css/login.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                    <div class="card card-signin my-5">
-                        <div class="card-body">
-                            <a href="principal.php">
-                                <img id="logo" src="../img/upomarket.png" alt="Logo de UPOMarket"/>
-                            </a>
-                            <h4 class="card-title text-center">Inicio de sesión</h4>
-                            <form class="form-signin">
-                                <div class="form-label-group">
-                                    <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
-                                </div>
-                                <br />
-                                <div class="form-label-group">
-                                    <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
-                                </div>
-                                <br />
-                                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Iniciar Sesión</button>
-                            </form>
+    <html>
+        <head>
+            <title>Iniciar Sesión</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="../frameworks/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+            <link href="../css/login.css" rel="stylesheet" type="text/css"/>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                        <div class="card card-signin my-5">
+                            <div class="card-body">
+                                <a href="principal.php">
+                                    <img id="logo" src="../img/upomarket.png" alt="Logo de UPOMarket"/>
+                                </a>
+                                <h4 class="card-title text-center">Inicio de sesión</h4>
+                                <form class="form-signin">
+                                    <div class="form-label-group">
+                                        <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
+                                    </div>
+                                    <br />
+                                    <div class="form-label-group">
+                                        <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                                    </div>
+                                    <br />
+                                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Iniciar Sesión</button>
+                                    <br />
+                                    <p>¿Aún no tienes una cuenta? <a href="signUp.php">¡Regístrate!</a></p>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
+
+    <?php
+}
+?>
+
+<?php
+session_start();
+if (!existeSesion()) {
+    cargarLogin();
+} else {
+    header('Location: ./principal.php');
+}
+?>
