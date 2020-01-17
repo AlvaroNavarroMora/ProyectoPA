bc5c800f5b6f713622c972dc2cde61df49063b19
Amalio <amaliocabeza.16@gmail.com>
2020-01-17 19:56:41 +0100

    Mejoras en aniadir Productos HTML y CSS. Creación de UtilsProducto php.
    Utils Producto contiene funciones interesantes a la hora de buscar productos y categorias.

---
+++ UPOMarket/php/utils/utilsProductos.php
@@ -0,0 +1,57 @@
+<?php
+include 'manejadorBD.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function listarCategorias() {
+    $query = "SELECT nombre FROM categorias";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
+
+/* Lista las categorías que tiene un producto */
+
+function listarCategoriasDeProducto($producto) {
+    $query = "SELECT * FROM `categorias` as c, `categorias_productos` as ca WHERE c.nombre=ca.nombre_categoria AND ca.id_producto=$producto;";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
+
+/* Lista los productos que pertenecen a una categoría X */
+
+function listarProductosDeCategoria($categoria) {
+    $query = "SELECT * FROM `productos` as p, `categorias_productos` as ca WHERE p.id=ca.id_producto AND ca.nombre_categoria='$categoria';";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
+
+/* order Type debe ser ASC (ascendente) o DESC (Descendente) */
+
+function listarProductosPorPrecio($orderType = "ASC") {
+    $query = "SELECT * FROM `productos` ORDER by `precio` $orderType";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
+
+/* order Type debe ser ASC (ascendente) o DESC (Descendente) */
+
+function listarProductosPorPrecioCategoria($orderType = "ASC") {
+    $query = "SELECT * FROM `productos` as p, `categorias_productos` as ca WHERE p.id=ca.id_producto AND ca.nombre_categoria='$categoria' ORDER by `precio` $orderType";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
+/*Email de usuario que queremos buscar*/
+function productosDeUsuario($email){
+    $query = "SELECT * FROM `productos` WHERE `email_vendedor`=$email";
+    $result = ejecutarConsulta($query);
+    $lista = mysqli_fetch_all($result);
+    return $lista;
+}
