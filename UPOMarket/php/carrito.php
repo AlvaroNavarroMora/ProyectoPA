6e65e6ac674983306e11474249d8b4c298f9618a
Alvaro <alvaronavarromora@gmail.com>
2020-01-21 23:56:49 +0100

    Carrito primera versión

--- UPOMarket/php/carrito.php
+++ UPOMarket/php/carrito.php
@@ -1,93 +1,117 @@
 <?php
+include "./utils/sesionUtils.php";
+include "./utils/manejadorBD.php";
+include './utils/encriptar.php';
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
+if (isset($_SESSION['email'])) {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
 
-        <title>UPOMarket-Inicio</title>
-        <link href="../frameworks/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../css/shop-homepage.css" rel="stylesheet">
-        <link href="../css/header.css" rel="stylesheet">
-        <link href="../css/footer.css" rel="stylesheet">
-        <link href="../css/principal.css" rel="stylesheet">
-        <link href="../css/carrito.css" rel="stylesheet" type="text/css"/>
-        <script src="../frameworks/jquery/jquery.min.js"></script>
-        <script src="../frameworks/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script><!-- Para que se vean los logos -->
-        <script src="../js/carrito.js" type="text/javascript"></script>
-    </head>
+            <title>UPOMarket-Inicio</title>
+            <link href="../frameworks/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+            <link href="../css/shop-homepage.css" rel="stylesheet">
+            <link href="../css/header.css" rel="stylesheet">
+            <link href="../css/footer.css" rel="stylesheet">
+            <link href="../css/principal.css" rel="stylesheet">
+            <link href="../css/carrito.css" rel="stylesheet" type="text/css"/>
+            <script src="../frameworks/jquery/jquery.min.js"></script>
+            <script src="../frameworks/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="https://kit.fontawesome.com/a076d05399.js"></script><!-- Para que se vean los logos -->
+            <script src="../js/carrito.js" type="text/javascript"></script>
+        </head>
 
-    <body>
-        <?php
-        include './header.php';
-        ?>
-        <!-- Page Content -->
-        <main class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <img id="logo_main" class="img-fluid" src="../img/upomarket.png" alt="upomarket">
-                    <nav class="list-group">
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="list-group-item">Category 1</a></li>
-                            <li><a href="#" class="list-group-item">Category 2</a></li>
-                            <li><a href="#" class="list-group-item">Category 3</a></li>
-                        </ul>
-                    </nav>
+        <body>
+            <?php
+            include './header.php';
+            ?>
+            <!-- Page Content -->
+            <main class="container">
+                <div class="row">
 
-                </div>
-                <!-- /.col-lg-3 -->
 
-                <div class="col-lg-3">
-                    <div class="row">
-                        <div class="divCarrito">
-                            <h3>Mi carrito</h3>
-                            <table id="productos" class="table table-striped table-bordered dataTable" style="width:100%">
-                                <thead>
+
+                    <div class="divCarrito">
+                        <h3>Mi carrito</h3>
+                        <?php
+                        if (!isset($_SESSION['carrito']) || empty($_SESSION['carrito'])) {
+                            echo "<div class='alert alert-success'>El carrito está vacío.</div>";
+                        } else {
+                            ?>
+                            <form method="post" action="./utils/anadirCarrito.php">
+                                <table id="tableProductos" class="table table-light">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Descripción</th>
+                                            <th>Imagen</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th>Subtotal </th>
+                                            <th>Eliminar </th>
+                                        </tr>
+                                        <?php
+                                        $i = 0;
+                                        foreach ($_SESSION['carrito'] as $indice => $producto) {
+                                            $query = "SELECT nombre, descripcion, precio, imagen FROM productos WHERE id='" . $producto['id'] . "'";
+                                            $result = ejecutarConsulta($query);
+                                            if (mysqli_num_rows($result) > 0) {
+                                                $row = mysqli_fetch_array($result);
+                                                if ($row['nombre'] == $producto['nombre']) {
+                                                    echo "<td>" . $row['nombre'] . "</td>";
+                                                    echo "<td>" . $row['descripcion'] . "</td>";
+                                                    echo "<td><img src='" . $row['imagen'] . "' alt='Imagen de producto' /></td>";
+                                                    echo "<td>" . $row['precio'] . "</td>";
+                                                    echo "<td><input name='cantidad" . $i . "' type='number' id='cantidad" . $i . "' value='" . $producto['cantidad'] . "'/></td>";
+                                                    echo "<td id ='subtotal" . $i . "'></td>";
+                                                    echo '<input type="hidden" name="idProducto' . $i . '" value="' . encriptar($producto['id']) . '">';
+                                                    echo "<td><button  id ='btnEliminarCarrito" . $i . "' name='btnEliminarCarrito' class='btn btn-danger' type='submit' value='" . $i . "' >Eliminar</button></td>";
+                                                }
+                                            }
+                                        }
+                                        ?>
+                                    </thead>
                                     <tr>
-                                        <th>Producto</th>
-                                        <th>Cantidad</th>
+                                        <td colspan="4"><h5>Total:</h5></td>
+                                        <td id="precioTotalCarrito" colspan="3"><?php echo number_format(345.293, 2); ?>€</td>
                                     </tr>
-                                </thead>
-                            </table>
-                        </div>
+                                </table>
+                            </form>
+                            <?php
+                        }
+                        ?>
+
                     </div>
+
+                    <!-- /.row -->
+
+
                 </div>
                 <!-- /.row -->
-
-                <div class="col-lg-3">
-                    <div class="row">
-                        <div class="divCarrito">
-                            <h4>Precio Total:</h4>
-                            <p>50€</p>
-                        </div>
+                <div class="row">
+                    <div class="divCarrito">
+                        <h4>Procesar Compra:</h4>
+                        <p>Dirección</p>
                     </div>
                 </div>
-                
-                
-                <div class="col-lg-3">
-                    <div class="row">
-                        <div class="divCarrito">
-                            <h4>Procesar Compra:</h4>
-                            <p>Dirección</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.col-lg-9 -->
+            </main>
+            <!-- /.container -->
+            <?php
+            include '../html/footer.html';
+            ?>
+        </body>
 
-            </div>
-            <!-- /.row -->
+    </html>
 
-        </main>
-        <!-- /.container -->
-        <?php
-        include '../html/footer.html';
-        ?>
-    </body>
-
-</html>
+    <?php
+} else {
+    header('Location: ./principal.php');
+    ;
+}
+?>
--- UPOMarket/php/utils/anadirCarrito.php
+++ UPOMarket/php/utils/anadirCarrito.php
@@ -1,29 +1,84 @@
 <?php
+
+session_start();
+if (isset($_SESSION['email'])) {
     include './encriptar.php';
-    if(isset($_POST['btnAgregarCarrito'])){
-        if(isset($_POST['id'])){
+    if (isset($_POST['btnAgregarCarrito'])) {
+        if (isset($_POST['id'])) {
             $idProv = desencriptar($_POST['id']);
-            if(is_numeric($idProv)){
+            if (is_numeric($idProv)) {
                 $id = $idProv;
-            }else{
+            } else {
                 $errores[] = "Parece que algo ha ido mal...";
             }
-        }else{
+        } else {
             $errores[] = "Parece que algo ha ido mal...";
         }
-        if(isset($_POST['nombre'])){
+        if (isset($_POST['nombre'])) {
             $nombreProv = desencriptar($_POST['nombre']);
-            $nombre = filter_var($nombreProv, FILTER_SANITIZE_MAGIC_QUOTES); 
-        }else{
+            $nombre = filter_var($nombreProv, FILTER_SANITIZE_MAGIC_QUOTES);
+        } else {
             $errores[] = "Parece que algo ha ido mal...";
         }
+
+        if (isset($errores)) {
+            alert("Parece que algo ha ido mal...");
+            header('Location: ../principal.php');
+        } else {
+            $producto = array(
+                'id' => $id,
+                'nombre' => $nombre,
+                'cantidad' => 1
+            );
+            if (!isset($_SESSION['carrito'])) {
+                $_SESSION['carrito'][0] = $producto;
+            } else {
+                $index = count($_SESSION['carrito']);
+                $_SESSION['carrito'][$index] = $producto;
+            }
+            //Volver a la página del produco--------------------------------------------------------------------
+            header('Location: ../principal.php'); //Cambiar por volver a la página del producto del que venimos
+            //--------------------------------------------------------------------------------------------------
+        }
+    } else {
+        if (isset($_POST['btnEliminarCarrito'])) {
+            $valorBtn = filter_var($_POST['btnEliminarCarrito'], FILTER_SANITIZE_NUMBER_INT);
+            if (is_numeric($valorBtn)) {
+                $index = 0;
+                $enc = false;
+                while (!$enc) {
+                    if ($index == $_POST['btnEliminarCarrito']) {
+                        $enc = true;
+                    } else {
+                        $index++;
+                    }
+                }
+                $idProductoProv = desencriptar($_POST['idProducto' . $index]);
+                $idProducto = filter_var($idProductoProv, FILTER_SANITIZE_NUMBER_INT);
+                if (is_numeric($idProducto)) {
+                    foreach ($_SESSION['carrito'] as $indice => $producto) {
+                        if ($producto['id'] == $idProducto) {
+                            unset($_SESSION['carrito'][$indice]);
+                        }
+                    }
+                    header('Location: ../carrito.php');
+                } else {
+                    $errores[] = "Parece que algo ha ido mal...";
+                }
+            } else {
+                $errores[] = "Parece que algo ha ido mal...";
+            }
+            if (isset($errores)) {
+                alert("Parece que algo ha ido mal...");
+                header('Location: ../principal.php');
+            }
+        } else {
+            header('Location: ../principal.php');
+        }
     }
-    if(isset($errores)){
-        alert("Parece que algo ha ido mal...");
-        header('Location: ./principal.php');
-    }else{
-        
-    }
-    
+} else {
+    alert("No existe ninguna sesión activa.");
+    header('Location: ../principal.php');
+}
 ?>
 
