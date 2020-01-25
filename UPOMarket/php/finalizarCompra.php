3e94bdc9ae997d68a5bbd0c1441f5d463dc18239
Alvaro <alvaronavarromora@gmail.com>
2020-01-25 15:48:27 +0100

    Compra procesada y finalizada

--- UPOMarket/php/finalizarCompra.php
+++ UPOMarket/php/finalizarCompra.php
@@ -1,20 +1,144 @@
 <?php
-$clientID = "Aag_BV9saCzCn3jZU7nRT-_qMd-sJuXnc9VKSeM5li-IXLAGDi2zUsiRtPpTu3Tvr46fIq9Ce6KSjkug";
-$secret = "EGLPpU61Vabi5eKBq24_3A0YufctYwuDDMudoj38vU6hgKxEW1OF2zWhuRjsG__FOOW2VBjBP7oAk3E-";
+include "./utils/sesionUtils.php";
+include './utils/encriptar.php';
+include './utils/utilsProductos.php';
+session_start();
+if (isset($_SESSION['email'])) {
+    if (isset($_GET[encriptar("clave")])) {
+        if (desencriptar($_GET[encriptar("clave")]) == "ProgramacionAvanzada") {
+            if (isset($_GET[encriptar("email")]) && isset($_GET[encriptar("direccion")])) {
+                $email = desencriptar($_GET[encriptar("email")]);
+                if ($email == $_SESSION['email']) {
+                    $direccion = desencriptar($_GET[encriptar("direccion")]);
+                    $i = 0;
+                    while (isset($_GET[encriptar("producto" . $i)]) && isset($_GET[encriptar("cantidad" . $i)])) {
+                        $productos[] = Array("id" => desencriptar($_GET[encriptar("producto" . $i)]),
+                            "cantidad" => desencriptar($_GET[encriptar("cantidad" . $i)]));
+                        $i++;
+                    }
+                    $query = "INSERT INTO pedidos (email_cliente, id_direccion, fecha) VALUES('" . $email . ", " . $direccion . "', '" . date("d/m/Y", time()) . "')";
+                    $link = openCon();
+                    $result = mysqli_query($link, $query);
+                    $idPedido = mysqli_insert_id($con);
+                    foreach ($productos as $producto) {
+                        $query = "INSERT INTO lineas_de_pedido (id_pedido, id_producto, cantidad, estado) "
+                                . "VALUES('$idPedido', '" . $producto['id'] . "', '" . $producto['cantidad'] . "', 'Procesado'";
+                        $result = mysqli_query($con, $query);
+                    }
+                    mysqli_close($link);
+                    unset($_SESSION['carrito']);
+                    unset($_SESSION['direccion']);
+                }
+            }
+        }
+    }
+}
+if (!isset($idPedido)) {
+    header('Location: ./principal.php');
+} else {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
 
-$login = curl_init("https://api.sandbox.paypal.com/v1/oauth2/token");
-curl_setopt($login, CURLOPT_RETURNTRANSFER, True);
-curl_setopt($login, CURLOPT_USERPWD, $clientID.":".$secret);
-curl_setopt($login, CURLOPT_RETURNTRANSFER, True);
-$respuesta = curl_exec();
-$objRespuesta = json_decode($respuesta);
+            <title>UPOMarket-Resumen de compra</title>
+            <link href="../frameworks/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+            <link href="../css/shop-homepage.css" rel="stylesheet">
+            <link href="../css/header.css" rel="stylesheet">
+            <link href="../css/footer.css" rel="stylesheet">
+            <link href="../css/principal.css" rel="stylesheet">
+            <link href="../css/carrito.css" rel="stylesheet" type="text/css"/>
+            <script src="../frameworks/jquery/jquery.min.js"></script>
+            <script src="../frameworks/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="https://kit.fontawesome.com/a076d05399.js"></script><!-- Para que se vean los logos -->
 
-$accessToken = $objRespuesta->access_token;
+        </head>
 
-$venta = curl_init("https://api.sandbox.paypal.com/v1/payments/payment/".$_GET['paymentID']);
+        <body>
 
-curl_setopt($venta, CURLOPT_HTTPHEADER, array("Content-Type: application/json","Authorization: Bearer ".$accessToken));
+            <?php
+            include './header.php';
+            ?>
+            <main class="container">
+                <div class="row">
+                    <div class="divCarrito">
+                        <h3>Compra finalizada</h3>
+                        <table id="tableProductos" class="table table-light">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Descripción</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                    <th>Subtotal </th>
+                                </tr>
+                            </thead>
+                            <!-- ----------Rellenar tabla con los datos de la compra---------------- -->
+                            <?php
+                            $i = 0;
+                            $subtotal = 0;
+                            $link = openCon();
+                            $total = 0;
+                            foreach ($productos as $producto) {
+                                $query = "SELECT nombre, descripcion, precio FROM productos WHERE id='".$producto['id']."'";
+                                $result = mysqli_query($link, $query);
+                                $row = mysqli_fetch_array($result);
+                                echo "<tr>";
+                                echo "<td>" . $row['nombre'] . "</td>";
+                                echo "<td>" . $row['descripcion'] . "</td>";
+                                echo "<td>" . $row['precio'] . "</td>";
+                                echo "<td>" . $producto['cantidad'] . "</td>";
+                                $subtotal = $row['precio'] * $producto['cantidad'];
+                                $total += $subtotal;
+                                echo "<td id ='subtotal" . $i . "'>$subtotal€</td>";
+                                echo "</tr>";
 
-$respuestaVenta = curl_exec($venta);
-print_r($respuestaVenta);
+                                $i++;
+                            }
+                            ?>
+
+                            <tr>
+                                <td colspan="3"><strong>Total:</strong></td>
+                                <td id="precioTotalCarrito" colspan="2"><?php echo number_format($total, 2); ?>€</td>
+                            </tr>
+                        </table>
+
+                        <div class="row">
+                            <div class="divCarrito">
+                                <strong>Dirección de envio:</strong>
+                                <br />
+                                <?php echo $direccion; ?>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="divCarrito">
+                                <strong>Descargar PDF</strong>
+                                <!-- --------Hacer que se pueda descargan un pdf------ -->
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </main>
+            <!-- /.container -->
+            <?php
+            include '../html/footer.html';
+            ?>
+
+        </body>
+
+    </html>
+    <?php
+}
 ?>
